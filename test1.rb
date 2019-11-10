a2346c9cdef254a1ad8efd924f10630e11f08e00
Fatima Zahra Chriha <30349896+FatiFleurCH@users.noreply.github.com>
2019-11-10 11:25:24 -0500

    Delete test1.rb

--- test1.rb
+++
@@ -1,7 +0,0 @@
-def money_my_child_will_have_when_they_retire(my_age)
-    child_age = my_age / 2
-    years_left_until_retirement = 65 - child_age
-    estimation_of_child_bank_account = child_age ** 2
-
-    estimation_of_child_bank_account * years_left_until_retirement
-end
